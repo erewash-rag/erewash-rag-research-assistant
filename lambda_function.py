81ed032e68d625ce356d585c75a6fe6c285c30bb
Matt Micklewright <matthewmicklewright@gmail.com>
2026-06-13 17:45:03 +0100

    Correct get item check

--- lambda_function.py
+++ lambda_function.py
@@ -135,16 +135,17 @@ def lambda_handler(event, _context):
     sources = scrape_erewash_council_news(erewash_council_news_url)
     for content, url in sources:
 
-        get_item_response = table.get_item(Key={"id": url, "sourceId": "erewash_council_news"})
-        if get_item_response["Item"] is None:
-            table.put_item(Item={
-                "id": url,
-                "sourceId": "erewash_council_news",
-                "dateAdded": datetime.now().strftime("%Y-%m-%d"),
-                "url": url,
-                "content": content or "",
-                "writtenAbout": False
-            })
+        get_item_response = table.get_item(Key={"id": url})
+        if "Item" in get_item_response and get_item_response["Item"] is not None:
+            continue
+        table.put_item(Item={
+            "id": url,
+            "sourceId": "erewash_council_news",
+            "dateAdded": datetime.now().strftime("%Y-%m-%d"),
+            "url": url,
+            "content": content or "",
+            "writtenAbout": False
+        })
     logger.info("Added %d sources to the DB", len(sources))
 
 
