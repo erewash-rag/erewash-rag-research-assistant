6a0b59e6dc223c8dc17f477f72cc08e7d5132974
Matt Micklewright <matthewmicklewright@gmail.com>
2026-06-13 18:24:22 +0100

    return status at end of lambda exec

--- lambda_function.py
+++ lambda_function.py
@@ -151,7 +151,7 @@ def lambda_handler(event, _context):
         sources_saved = sources_saved + 1
 
     logger.info("Added %d sources to the DB", sources_saved)
-
+    return {"statusCode": 200, "body": f"Added {sources_saved} sources to the DB"}
 
 if __name__ == "__main__":
     lambda_handler({}, None)
