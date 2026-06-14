eaf8f1cadc079cf0ef2c36a9926b9a8822697053
Matt Micklewright <matthewmicklewright@gmail.com>
2026-06-14 23:37:23 +0100

    Only do 1 scraper for tests

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ import os
 import db
 from config import get_property
 from scrapers import SCRAPERS
+from scrapers import erewash_council_news
 
 logger = logging.getLogger(__name__)
 logging.getLogger().setLevel(os.environ.get("logging_level", "INFO"))
@@ -11,20 +12,25 @@ logging.getLogger().setLevel(os.environ.get("logging_level", "INFO"))
 
 def lambda_handler(event, _context):
     total_saved = 0
-    for scraper in SCRAPERS:
-        sources_per_scraper = 0
-        logger.info("Scraping from %s...", scraper.SOURCE_ID)
-        base_url = event.get(f"{scraper.SOURCE_ID}_url") or get_property(f"{scraper.SOURCE_ID}_url")
-        sources = scraper.scrape(base_url)
-        for content, url in sources:
-            if db.save_source(url, scraper.SOURCE_ID, content):
-                sources_per_scraper += 1
-                total_saved += 1
-        logger.info("%d saved to DB for source %s", scraper.SOURCE_ID)
+    if event["erewash_council_news_url"] is not None:
+        perform_scrape(erewash_council_news, event["erewash_council_news_url"])
+    else:
+        for scraper in SCRAPERS:
+            total_saved += perform_scrape(scraper)
 
     logger.info("Added %d sources total to the DB", total_saved)
     return {"statusCode": 200, "body": f"Added {total_saved} sources to the DB"}
 
+def perform_scrape(scraper, single_url):
+    sources_per_scraper = 0
+    logger.info("Scraping from %s...", scraper.SOURCE_ID)
+    base_url = single_url or get_property(f"{scraper.SOURCE_ID}_url")
+    sources = scraper.scrape(base_url)
+    for content, url in sources:
+        if db.save_source(url, scraper.SOURCE_ID, content):
+            sources_per_scraper += 1
+    logger.info("%d saved to DB for source %s", sources_per_scraper, scraper.SOURCE_ID)
+    return sources_per_scraper
 
 if __name__ == "__main__":
     lambda_handler({}, None)
